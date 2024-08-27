a1b41ee3f71ffb31df47da384795c04bddb0001a
6uiwj <oterges00@gmail.com>
2024-08-28 02:23:47 +0900

    240828 - Stream API

---
+++ src/stream/StreamAssist.java
@@ -0,0 +1,32 @@
+package stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamAssist {
+    public static void main(String[] args) {
+
+        //Custormes 컬렉션에 임의의 5개 데이터 추가
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("Song", 45));
+        customers.add(new Customer("Kim", 33));
+        customers.add(new Customer("Park", 21));
+        customers.add(new Customer("Lee", 67));
+        customers.add(new Customer("Choi", 19));
+
+
+        //조건 1. 나이가 30 이상 사람
+        //조건 2. 오름차순 정렬,
+        //조건 3. 결과를 customer 객체가 아닌 문자열 데이터로 추출
+
+        //기존 방법
+        List<Customer> list = new ArrayList<>();
+        for (Customer customer : customers) {
+            if(customer.getAge() > 30) {
+                list.add(customer);
+            }
+        } //age 30> 추출
+
+
+    }
+}
